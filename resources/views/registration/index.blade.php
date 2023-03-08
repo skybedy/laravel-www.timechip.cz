3ff9bcf7cfc78813a1e8e4f79359c4a01d3411bc
skybedy <skybedy@gmail.com>
2023-03-08 11:49:29 +0100

    Upravy

--- resources/views/registration/index.blade.php
+++ resources/views/registration/index.blade.php
@@ -14,20 +14,51 @@
     
 
 
-<form class="well form-horizontal">
-    <div class="form-group">
-        <label class="col-md-4 control-label">First Name</label>  
-        <div class="col-md-4 inputGroupContainer">
-        <div class="input-group">
-        <span class="input-group-addon"><i class="glyphicon glyphicon-user"></i></span>
-        <input  name="first_name" placeholder="First Name" class="form-control"  type="text">
+<form action="" class="mt-2 flex flex-col lg:w-1/2 w-8/12">
+          <div class="flex flex-wrap items-stretch w-full mb-4 relative h-15 bg-white items-center rounded mb-6 pr-10">
+            <div class="flex -mr-px justify-center w-15 p-4">
+              <span
+                class="flex items-center leading-normal bg-white px-3 border-0 rounded rounded-r-none text-2xl text-gray-600"
+              >
+                <i class="fas fa-user-circle"></i>
+              </span>
+            </div>
+            <input
+              type="text"
+              class="flex-shrink flex-grow flex-auto leading-normal w-px flex-1 border-0 h-10 border-grey-light rounded rounded-l-none px-3 self-center relative  font-roboto text-xl outline-none"
+              placeholder="Username"
+            />
           </div>
-        </div>
-      </div>    
-
-
-
-</form>
+          <div class="flex flex-wrap items-stretch w-full relative h-15 bg-white items-center rounded mb-4">
+            <div class="flex -mr-px justify-center w-15 p-4">
+              <span
+                class="flex items-center leading-normal bg-white rounded rounded-r-none text-xl px-3 whitespace-no-wrap text-gray-600"
+                > 
+                <i class="fas fa-lock"></i>
+                  </span
+              >
+            </div>
+            <input
+              type="password"
+              class="flex-shrink flex-grow flex-auto leading-normal w-px flex-1 border-0 h-10 px-3 relative self-center font-roboto text-xl outline-none"
+              placeholder="Password"
+            />
+            <div class="flex -mr-px">
+              <span
+                class="flex items-center leading-normal bg-white rounded rounded-l-none border-0 px-3 whitespace-no-wrap text-gray-600"
+                >
+                <i class="fas fa-eye-slash"></i>
+                </span>
+            </div>
+          </div>
+          <a href="#" class="text-base text-white text-right font-roboto leading-normal hover:underline mb-6">Forget Password ?</a>
+          <a
+          href="/"
+            class="bg-blue-400 py-4 text-center px-17 md:px-12 md:py-4 text-white rounded leading-tight text-xl md:text-base font-sans mt-4 mb-20"
+          >
+            Login
+        </a>
+        </form>
 
 
 
