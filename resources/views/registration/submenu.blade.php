4d75ddcfc2d66a749851aeb1207e962359537d3d
skybedy <skybedy@gmail.com>
2023-03-25 14:50:56 +0100

    Zmena sablony

--- resources/views/registration/submenu.blade.php
+++ resources/views/registration/submenu.blade.php
@@ -1,6 +1,6 @@
 <div class="row mb-5">
     <div class="col-sm-11 text-end">
         <a class="btn  btn-outline-danger" href="{{ route('registration',['raceName' => $raceName, 'raceYear' => $raceYear,'raceId' => $raceId]) }}" role="button">Registrační formulář formulář</a>
-        <a class="btn  btn-outline-danger" href="{{ route('registration_list',['raceYear' => $raceYear,'raceId' => $raceId,'raceName' => $raceName]) }}" role="button">Seznam přihlášek</a>
+        <a class="btn  btn-outline-danger" href="{{ route('registration_list',['raceYear' => $raceYear,'raceId' => $raceId,'raceName' => $raceName]) }}" role="button">Seznam registrací</a>
     </div>
 </div>   
