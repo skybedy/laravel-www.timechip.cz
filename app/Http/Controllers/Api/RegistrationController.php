2af32fa9d0130f66f03493a45bce936ad5b6e28e
skybedy <skybedy@gmail.com>
2024-02-16 16:10:18 +0100

    160224hp

--- app/Http/Controllers/Api/RegistrationController.php
+++ app/Http/Controllers/Api/RegistrationController.php
@@ -7,27 +7,11 @@ use Illuminate\Support\Facades\DB;
 
 class RegistrationController extends Controller
 {
-    public function startNumberCheck()
-    {
 
-        $sql =  DB::table('kategorie_2023')
-        ->select('id_kategorie')
-        ->where('id_zavodu','=',21);
-        
+public function index()
+{
+    dd("Hello World!");
+}
 
-        return $sql->get();
-
-    }
-
-
-    public function startNumberChoiceEnduroCC($category_id)
-    {
-        return DB::table('ids_endurocc')
-        ->select('ids')
-        ->where('id_kategorie','=',$category_id)
-        ->where('volne','=',1)
-        ->orderBy('ids','ASC')->get();
-
-    }
 
 }
